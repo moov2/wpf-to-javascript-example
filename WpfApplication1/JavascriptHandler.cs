2f089f9edac80ff026ac66415ea67cd8cee474bd
JoeMoov2 <joe.berkley@moov2.com>
2015-04-17 12:27:18 +0100

    First project commit

---
+++ WpfApplication1/JavascriptHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisible(true)]
+    public class ObjectForScriptingHelper
+    {
+
+        MainWindow mExternalWPF;
+        public ObjectForScriptingHelper(MainWindow w)
+        {
+            this.mExternalWPF = w;
+        }
+        
+        // Methods here are able to be invoked from the 
+        // javascript using window.external.MethodName
+
+        public void SaveData(object JSON)
+        {
+            if(!System.IO.Directory.Exists("C:\\WPFBrowser"))
+                System.IO.Directory.CreateDirectory("C:\\WPFBrowser");
+
+            System.IO.File.WriteAllText("C:\\WPFBrowser\\data.JSON", JSON.ToString());
+        }
+
+        public string LoadData()
+        {
+            string r = "";
+            if(System.IO.File.Exists("C:\\WPFBrowser\\data.JSON"))
+            {
+                r = System.IO.File.ReadAllText("C:\\WPFBrowser\\data.JSON");
+            }
+            return r;
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // When the application loads set the registry key to force the app to
+            // render in IE 9 - Use a currentuser key so that admin rights
+            // are not needed. See README for other version numbers.
+
+            // If doing a release remove the vshost from the file name.
+            
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION");
+            key.SetValue("WPF Javascript CSharp interface.exe.vshost.exe", "9000", Microsoft.Win32.RegistryValueKind.DWord);
+            key.Close();
+
+            InitializeComponent();
+            
+            //Set this up so that the web browser exposes a scripting COM interface to the JavaScript
+            ObjectForScriptingHelper helper = new ObjectForScriptingHelper(this);
+            this.wbMain.ObjectForScripting = helper;
+            
+        }
+    }
+}
